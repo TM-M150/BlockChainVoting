2e23243678d04ea7a7160d0b6a112356eff81016
Tommy Maro <tommymaro213@gmail.com>
2023-04-04 17:22:30 -0500

    Initial Commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,10 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 import { createError } from "../utils/error.js";
+import { generateKeyPair } from "../utils/keygen.js";
 
-const registerUser = async function (req, res, next) {
+export const registerUser = async function (req, res, next) {
   try {
     // Check if the user already exists
     const existingUser = await User.findOne({ Email: req.body.Email });
@@ -12,13 +14,16 @@ const registerUser = async function (req, res, next) {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.Password, salt);
 
+    // Generate a new key pair
+    const { publicKey, privateKey } = generateKeyPair();
+
     // Create a new user
     const newUser = new User({
       Name: req.body.Name,
       Email: req.body.Email,
       Password: hash,
-      PublicKey: "XXXXXXXXXXXXXX", // Replace with generated public key
-      PrivateKey: "XXXXXXXXXXXXXX", // Replace with generated private key
+      PublicKey: publicKey,
+      PrivateKey: privateKey,
     });
 
     // Save the user to the database
@@ -32,4 +37,79 @@ const registerUser = async function (req, res, next) {
   }
 };
 
-export { registerUser };
+export const loginUser = async function (req, res, next) {
+  try {
+    // Find the user in the database by email
+    const user = await User.findOne({ Email: req.body.Email });
+    if (!user) return next(createError(401, "Invalid email or password"));
+
+    // Compare the password hash with the input password
+    const passwordMatch = await bcrypt.compare(
+      req.body.Password,
+      user.Password
+    );
+    if (!passwordMatch)
+      return next(createError(401, "Invalid email or password"));
+
+    // Generate a JWT token
+    const token = jwt.sign({ UserId: user.UserId }, process.env.JWT);
+
+    // Return the token in the response
+    res.status(200).json({ token });
+  } catch (error) {
+    // Handle errors
+    next(error);
+  }
+};
+
+export const updateUser = async function (req, res, next) {
+  try {
+    // Find the user in the database by ID
+    const user = await User.findById(req.params.userId);
+    if (!user) return next(createError(404, "User not found"));
+
+    // Update user's name and email
+    user.Name = req.body.Name;
+    user.Email = req.body.Email;
+
+    // Save the updated user to the database
+    await user.save();
+
+    // Return success response
+    res.status(200).json(user);
+  } catch (error) {
+    // Handle errors
+    next(error);
+  }
+};
+
+export const getUser = async function (req, res, next) {
+  try {
+    // Find the user in the database by ID
+    const user = await User.findById(req.params.userId);
+    if (!user) return next(createError(404, "User not found"));
+
+    // Return user data in the response
+    res.status(200).json(user);
+  } catch (error) {
+    // Handle errors
+    next(error);
+  }
+};
+
+export const deleteUser = async function (req, res, next) {
+  try {
+    // Find the user in the database by ID
+    const user = await User.findById(req.params.userId);
+    if (!user) return next(createError(404, "User not found"));
+
+    // Delete the user from the database
+    await user.deleteOne();
+
+    // Return success response
+    res.status(200).json("User deleted successfully");
+  } catch (error) {
+    // Handle errors
+    next(error);
+  }
+};
---
+++ server/utils/keygen.js
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+
+function generateKeyPair() {
+  const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: "spki",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs8",
+      format: "pem",
+      cipher: "aes-256-cbc",
+      passphrase: "top secret",
+    },
+  });
+
+  return { publicKey, privateKey };
+}
+
+module.exports = { generateKeyPair };
